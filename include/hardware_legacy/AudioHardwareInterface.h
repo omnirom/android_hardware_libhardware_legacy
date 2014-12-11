e6d7aa842514ab70ec9e5433b77b5293addd034f
rooque <victor.rooque@gmail.com>
2014-12-11 16:53:39 -0200

    libhardware_legacy: Workaround for old libaudio
    PT 1:
    Revert "openOutputStreamWithFlags" pure virtual abstract classe.
    
    PT 2:
    Added ifdef -- > USE_OLD_LIBAUDIO
    
    Change-Id: I88059599ba0731c64db6c8ca9b6390721db78545

--- include/hardware_legacy/AudioHardwareInterface.h
+++ include/hardware_legacy/AudioHardwareInterface.h
@@ -265,15 +265,25 @@ public:
                                 uint32_t *channels=0,
                                 uint32_t *sampleRate=0,
                                 status_t *status=0) = 0;
+#ifdef USE_OLD_LIBAUDIO
     virtual AudioStreamOut* openOutputStreamWithFlags(
                                 uint32_t devices,
                                 audio_output_flags_t flags=(audio_output_flags_t)0,
                                 int *format=0,
                                 uint32_t *channels=0,
                                 uint32_t *sampleRate=0,
-                                status_t *status=0) = 0;
+                                status_t *status=0);
     virtual    void        closeOutputStream(AudioStreamOut* out) = 0;
-
+#else
+    virtual AudioStreamOut* openOutputStreamWithFlags(
+                                uint32_t devices,
+                                audio_output_flags_t flags=(audio_output_flags_t)0,
+                                int *format=0,
+                                uint32_t *channels=0,
+                                uint32_t *sampleRate=0,
+                                status_t *status=0)= 0;
+    virtual    void        closeOutputStream(AudioStreamOut* out) = 0;
+#endif
     /** This method creates and opens the audio hardware input stream */
     virtual AudioStreamIn* openInputStream(
                                 uint32_t devices,
